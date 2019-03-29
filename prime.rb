31a9d5637630f54d5540077373af41910e2e859a
Tristan Jackson <tristan@audreysmonsters.com>
2019-03-29 13:07:15 -0500

    prime numbers

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(n)
+    # corner cases
+    if n <= 1
+        return false
+    end
+    if n <= 3 
+        return true
+    end
+    # Check from 2 to n-1
+    for i in 2..n do
+        if n % 2 == 0 || n % 3 == 0
+            return false
+        end
+    i = 5
+    while i * i <= n
+        if n % i == 0 || n % (i + 2) == 0
+            return false
+        end
+        i = i + 6
+        end
+    return true
+    end
+end
